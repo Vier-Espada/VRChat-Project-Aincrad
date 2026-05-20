466860dfc094d7cab32b40de2e11d2e7428db734
Vier Espada <pohaltpk@gmail.com>
2026-05-20 10:43:58 +0900

    Update detail.js

--- page/monster/detail/detail.js
+++ page/monster/detail/detail.js
@@ -2,11 +2,21 @@ function showTab(id){
 
     document
     .querySelectorAll(".tab-content")
-    .forEach(tab=>tab.classList.remove("active"));
+    .forEach(tab =>
+        tab.classList.remove("active")
+    );
+
+    document
+    .querySelectorAll(".tabs button")
+    .forEach(btn =>
+        btn.classList.remove("active")
+    );
 
     document
     .getElementById(id)
     .classList.add("active");
+
+    event.target.classList.add("active");
 }
 
 
@@ -19,7 +29,9 @@ function changeMap(map,btn){
 
     document
     .querySelectorAll(".floor-btn")
-    .forEach(b=>b.classList.remove("active"));
+    .forEach(b =>
+        b.classList.remove("active")
+    );
 
     btn.classList.add("active");
 
@@ -33,4 +45,5 @@ function changeMap(map,btn){
         box.innerHTML =
         "マップが存在しません";
     }
+
 }
